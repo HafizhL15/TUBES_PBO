052bb664c481d0c9c277c29de7de5ddaa0aa7e6a
HafizhL15 <muhhafizh33@gmail.com>
2021-06-25 18:55:27 +0700

    five

--- pbo/mywebsite/settings.py
+++ pbo/mywebsite/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'mywebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tes_pbo',
-        'USER' : 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'library_system'
     }
 }
 
