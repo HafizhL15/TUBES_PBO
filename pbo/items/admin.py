ee2c97d51ffbe8b21d21a8d0b39e8d696d5b7b83
HafizhL15 <muhhafizh33@gmail.com>
2021-07-06 19:42:35 +0700

    delete

--- pbo/items/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Barang
-
-admin.site.register(Barang)
--- pbo/library/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Perpustakaan
-
-admin.site.register(Perpustakaan)
